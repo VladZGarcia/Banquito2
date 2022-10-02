32cda754acbfacebeaed39f17f53873165799795
HUGO_PC <vladi.garcia@gmail.com>
2022-10-02 19:29:57 +0200

    eleventh commit PC

--- app/src/main/java/com/example/banquito2/BanquitoStartActivity.kt
+++ app/src/main/java/com/example/banquito2/BanquitoStartActivity.kt
@@ -35,7 +35,7 @@ class BanquitoStartActivity : AppCompatActivity() {
             val cardButton3 = findViewById<ImageView>(R.id.cardButton3)
             val cardButton4 = findViewById<ImageView>(R.id.cardButton4)
             val cardButton5 = findViewById<ImageView>(R.id.cardButton5)
-            val betEditText = findViewById<EditText>(R.id.betEditText)
+            betView = findViewById(R.id.betEditText)
 
 
              name = intent.getStringExtra("nameText")
@@ -46,25 +46,26 @@ class BanquitoStartActivity : AppCompatActivity() {
 
             player1 = Players("$name ", 100, false)
 
-        var button1Card = deck.newRndCard()
-        button1Card.pile = 1
-        piles.add(button1Card)
-        var button2Card = deck.newRndCard()
-        button2Card.pile = 2
-        piles.add(button2Card)
-        var button3Card = deck.newRndCard()
-        button3Card.pile = 3
-        piles.add(button3Card)
-        var button4Card = deck.newRndCard()
-        button4Card.pile = 4
-        piles.add(button4Card)
-        var button5Card = deck.newRndCard()
-        button5Card.pile = 5
-        piles.add(button5Card)
+
 
 
 
         if(firstRound){
+            var button1Card = deck.newRndCard()
+            button1Card.pile = 1
+            piles.add(button1Card)
+            var button2Card = deck.newRndCard()
+            button2Card.pile = 2
+            piles.add(button2Card)
+            var button3Card = deck.newRndCard()
+            button3Card.pile = 3
+            piles.add(button3Card)
+            var button4Card = deck.newRndCard()
+            button4Card.pile = 4
+            piles.add(button4Card)
+            var button5Card = deck.newRndCard()
+            button5Card.pile = 5
+            piles.add(button5Card)
             cardButton1.setOnClickListener {
                 val animation1 = AnimationUtils.loadAnimation(this, R.anim.bounce)
                 cardButton1.startAnimation(animation1)
@@ -189,117 +190,138 @@ class BanquitoStartActivity : AppCompatActivity() {
         }
         // Betting time!!
         infoTextView.setOnClickListener {
-            infoTextView.text = "Dags att spela om Banquitos!\n Hur mycket Banquitos vill du spela om?"
+            var resultText = "Dags att spela om Banquitos!\n "
+
             cardButton1.setImageResource(R.drawable.peter_river)
             cardButton2.setImageResource(R.drawable.peter_river)
             cardButton3.setImageResource(R.drawable.peter_river)
             cardButton4.setImageResource(R.drawable.peter_river)
             cardButton5.setImageResource(R.drawable.peter_river)
 
+            infoTextView.text = "$resultText \n Hur mycket vill du spela om?"
+
+            cardButton1.setImageResource(R.drawable.peter_river)
+            cardButton2.setImageResource(R.drawable.peter_river)
+            cardButton3.setImageResource(R.drawable.peter_river)
+            cardButton4.setImageResource(R.drawable.peter_river)
+            cardButton5.setImageResource(R.drawable.peter_river)
+            betView.setOnEditorActionListener(TextView.OnEditorActionListener { v, id, event ->
+                if (id == EditorInfo.IME_ACTION_DONE) {
+                    var betInput = betView.text.toString() ?: null
+                    bet = betInput!!.toInt()
+                    player1.banquitoBet = bet
+
+                    deck.createDeck()
+                    infoTextView.text="\n Nya kort!"
+                    //från BuggKod
+                    for (player in playerList.players) {
+                        if(!player.bank) {
+                            var playerbet = (1..5).random()
+                            player.banquitoBet = playerbet
+                            resultText = "${infoTextView.text}\n ${player.name} betar ${player.banquitoBet} Banquitos"
+                            infoTextView.text = resultText
+                        }
+                    }
+                    //
+
+                    var button1Card = deck.newRndCard()
+                    button1Card.pile = 1
+                    piles.add(button1Card)
+                    var button2Card = deck.newRndCard()
+                    button2Card.pile = 2
+                    piles.add(button2Card)
+                    var button3Card = deck.newRndCard()
+                    button3Card.pile = 3
+                    piles.add(button3Card)
+                    var button4Card = deck.newRndCard()
+                    button4Card.pile = 4
+                    piles.add(button4Card)
+                    var button5Card = deck.newRndCard()
+                    button5Card.pile = 5
+                    piles.add(button5Card)
+
+                    cardButton1.setOnClickListener {
+                        val animation1 = AnimationUtils.loadAnimation(this, R.anim.bounce)
+                        cardButton1.startAnimation(animation1)
+                        cardButton1.setImageResource(button1Card.image)
+                        valdPile = 0
+
+                        if (piles.size == 5) {
+                            cardButton2.callOnClick()
+                            cardButton3.callOnClick()
+                            cardButton4.callOnClick()
+                            cardButton5.callOnClick()
+                        }
+                    }
+
+                    cardButton2.setOnClickListener {
+                        val animation2 = AnimationUtils.loadAnimation(this, R.anim.bounce)
+                        cardButton2.startAnimation(animation2)
+                        cardButton2.setImageResource(button2Card.image)
+                        valdPile = 1
+
+                        if (piles.size == 5) {
+                            cardButton1.callOnClick()
+                            cardButton3.callOnClick()
+                            cardButton4.callOnClick()
+                            cardButton5.callOnClick()
+                        }
+                    }
+                    cardButton3.setOnClickListener {
+                        val animation3 = AnimationUtils.loadAnimation(this, R.anim.bounce)
+                        cardButton3.startAnimation(animation3)
+                        cardButton3.setImageResource(button3Card.image)
+                        valdPile = 2
+
+                        if (piles.size == 5) {
+                            cardButton1.callOnClick()
+                            cardButton2.callOnClick()
+                            cardButton4.callOnClick()
+                            cardButton5.callOnClick()
+                        }
+                    }
+                    cardButton4.setOnClickListener {
+                        val animation4 = AnimationUtils.loadAnimation(this, R.anim.bounce)
+                        cardButton4.startAnimation(animation4)
+                        cardButton4.setImageResource(button4Card.image)
+                        valdPile = 3
+
+                        if (piles.size == 5) {
+                            cardButton1.callOnClick()
+                            cardButton3.callOnClick()
+                            cardButton2.callOnClick()
+                            cardButton5.callOnClick()
+                        }
+                    }
+                    cardButton5.setOnClickListener {
+                        val animation5 = AnimationUtils.loadAnimation(this, R.anim.bounce)
+                        cardButton5.startAnimation(animation5)
+                        cardButton5.setImageResource(button5Card.image)
+                        valdPile = 4
+
+                        if (piles.size == 5) {
+                            cardButton1.callOnClick()
+                            cardButton3.callOnClick()
+                            cardButton4.callOnClick()
+                            cardButton2.callOnClick()
+                        }
+                    }
+                    true
+                } else false
+            })
+
         }
 
 
-            while (bet != 0) {
-
-                infoTextView.text = "Dags att spela!\n Hur mycket vill du spela om?"
-
-                cardButton1.setImageResource(R.drawable.peter_river)
-                cardButton2.setImageResource(R.drawable.peter_river)
-                cardButton3.setImageResource(R.drawable.peter_river)
-                cardButton4.setImageResource(R.drawable.peter_river)
-                cardButton5.setImageResource(R.drawable.peter_river)
-                betEditText.setOnEditorActionListener(TextView.OnEditorActionListener { v, id, event ->
-                    if (id == EditorInfo.IME_ACTION_DONE) {
-                        var betInput = betEditText.text.toString() ?: null
-                        bet = betInput!!.toInt()
-                        true
-                    } else false
-                })
-                var button1Card = deck.newRndCard()
-                button1Card.pile = 1
-                piles.add(button1Card)
-                var button2Card = deck.newRndCard()
-                button2Card.pile = 2
-                piles.add(button2Card)
-                var button3Card = deck.newRndCard()
-                button3Card.pile = 3
-                piles.add(button3Card)
-                var button4Card = deck.newRndCard()
-                button4Card.pile = 4
-                piles.add(button4Card)
-                var button5Card = deck.newRndCard()
-                button5Card.pile = 5
-                piles.add(button5Card)
+           // while (bet != 0) {
 
 
 
-                cardButton1.setOnClickListener {
-                    val animation1 = AnimationUtils.loadAnimation(this, R.anim.bounce)
-                    cardButton1.startAnimation(animation1)
-                    cardButton1.setImageResource(button1Card.image)
-                    valdPile = 0
 
-                    if (piles.size == 5) {
-                        cardButton2.callOnClick()
-                        cardButton3.callOnClick()
-                        cardButton4.callOnClick()
-                        cardButton5.callOnClick()
-                    }
-                }
 
-                cardButton2.setOnClickListener {
-                    val animation2 = AnimationUtils.loadAnimation(this, R.anim.bounce)
-                    cardButton2.startAnimation(animation2)
-                    cardButton2.setImageResource(button2Card.image)
-                    valdPile = 1
 
-                    if (piles.size == 5) {
-                        cardButton1.callOnClick()
-                        cardButton3.callOnClick()
-                        cardButton4.callOnClick()
-                        cardButton5.callOnClick()
-                    }
-                }
-                cardButton3.setOnClickListener {
-                    val animation3 = AnimationUtils.loadAnimation(this, R.anim.bounce)
-                    cardButton3.startAnimation(animation3)
-                    cardButton3.setImageResource(button3Card.image)
-                    valdPile = 2
 
-                    if (piles.size == 5) {
-                        cardButton1.callOnClick()
-                        cardButton2.callOnClick()
-                        cardButton4.callOnClick()
-                        cardButton5.callOnClick()
-                    }
-                }
-                cardButton4.setOnClickListener {
-                    val animation4 = AnimationUtils.loadAnimation(this, R.anim.bounce)
-                    cardButton4.startAnimation(animation4)
-                    cardButton4.setImageResource(button4Card.image)
-                    valdPile = 3
-
-                    if (piles.size == 5) {
-                        cardButton1.callOnClick()
-                        cardButton3.callOnClick()
-                        cardButton2.callOnClick()
-                        cardButton5.callOnClick()
-                    }
-                }
-                cardButton5.setOnClickListener {
-                    val animation5 = AnimationUtils.loadAnimation(this, R.anim.bounce)
-                    cardButton5.startAnimation(animation5)
-                    cardButton5.setImageResource(button5Card.image)
-                    valdPile = 4
-
-                    if (piles.size == 5) {
-                        cardButton1.callOnClick()
-                        cardButton3.callOnClick()
-                        cardButton4.callOnClick()
-                        cardButton2.callOnClick()
-                    }
-                }
-            }
+            //}
     }
 
     fun removePile() : Cards {
@@ -344,7 +366,7 @@ class BanquitoStartActivity : AppCompatActivity() {
         resultText =  "${infoTextView.text}\n${highestVal.name} har högsta kort och är Banken\n Tryck på skärmen!"
         infoTextView.text = resultText
 
-        return
+
 
 
     }
